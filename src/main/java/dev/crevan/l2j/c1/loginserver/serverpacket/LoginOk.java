7678fd9eb325cf4ae14dedcaca4ed8291d94bb0f
Crevan <a.siverskii@gmail.com>
2023-07-31 11:41:08 +0300

    [add] LoginOk

---
+++ src/main/java/dev/crevan/l2j/c1/loginserver/serverpacket/LoginOk.java
@@ -0,0 +1,21 @@
+package dev.crevan.l2j.c1.loginserver.serverpacket;
+
+public class LoginOk extends ServerBasePacket {
+
+    public LoginOk() {
+        writeC(0x03);
+        writeD(0x55555555);
+        writeD(0x44444444);
+        writeD(0x00);
+        writeD(0x00);
+        writeD(0x000003ea);
+        writeD(0x00);
+        writeD(0x00);
+        writeD(0x02);
+    }
+
+    @Override
+    public byte[] getContent() throws Exception {
+        return getBytes();
+    }
+}
