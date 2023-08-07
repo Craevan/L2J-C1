317e5ad46702d41fd263fc1e7d1ab53b1760becf
Crevan <a.siverskii@gmail.com>
2023-08-07 17:33:06 +0300

    [add] ClassId

---
+++ src/main/java/dev/crevan/l2j/c1/gameserver/model/ClassId.java
@@ -0,0 +1,72 @@
+package dev.crevan.l2j.c1.gameserver.model;
+
+public class ClassId {
+
+    public static final int fighter = 0x00;
+
+    public static final int warrior = 0x01;
+    public static final int gladiator = 0x02;
+    public static final int warlord = 0x03;
+    public static final int knight = 0x04;
+    public static final int paladin = 0x05;
+    public static final int darkAvenger = 0x06;
+    public static final int rogue = 0x07;
+    public static final int treasureHunter = 0x08;
+    public static final int hawkeye = 0x09;
+
+    public static final int mage = 0x0a;
+    public static final int wizard = 0x0b;
+    public static final int sorceror = 0x0c;
+    public static final int necromancer = 0x0d;
+    public static final int warlock = 0x0e;
+    public static final int cleric = 0x0f;
+    public static final int bishop = 0x10;
+    public static final int prophet = 0x11;
+
+    public static final int elvenFighter = 0x12;
+    public static final int elvenKnight = 0x13;
+    public static final int templeKnight = 0x14;
+    public static final int swordSinger = 0x15;
+    public static final int elvenScout = 0x16;
+    public static final int plainsWalker = 0x17;
+    public static final int silverRanger = 0x18;
+
+    public static final int elvenMage = 0x19;
+    public static final int elvenWizard = 0x1a;
+    public static final int spellsinger = 0x1b;
+    public static final int elementalSummoner = 0x1c;
+    public static final int oracle = 0x1d;
+    public static final int elder = 0x1e;
+
+    public static final int darkFighter = 0x1f;
+    public static final int palusKnight = 0x20;
+    public static final int shillienKnight = 0x21;
+    public static final int bladedancer = 0x22;
+    public static final int assassin = 0x23;
+    public static final int abyssWalker = 0x24;
+    public static final int phantomRanger = 0x25;
+
+    public static final int darkMage = 0x26;
+    public static final int darkWizard = 0x27;
+    public static final int spellhowler = 0x28;
+    public static final int phantomSummoner = 0x29;
+    public static final int shillienOracle = 0x2a;
+    public static final int shillenElder = 0x2b;
+
+    public static final int orcFighter = 0x2c;
+    public static final int orcRaider = 0x2d;
+    public static final int destroyer = 0x2e;
+    public static final int orcMonk = 0x2f;
+    public static final int tyrant = 0x30;
+
+    public static final int orcMage = 0x31;
+    public static final int orcShaman = 0x32;
+    public static final int overlord = 0x33;
+    public static final int warcryer = 0x34;
+
+    public static final int dwarvenFighter = 0x35;
+    public static final int scavenger = 0x36;
+    public static final int bountyHunter = 0x37;
+    public static final int artisan = 0x38;
+    public static final int warsmith = 0x39;
+}
