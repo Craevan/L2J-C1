0ab8acffd6275a12e3af005142d472a71f6e9ad4
Crevan <a.siverskii@gmail.com>
2023-11-22 22:25:21 +0300

    [add] L2Object model update

--- src/main/java/dev/crevan/l2j/c1/gameserver/model/L2Object.java
+++ src/main/java/dev/crevan/l2j/c1/gameserver/model/L2Object.java
@@ -3,15 +3,20 @@ package dev.crevan.l2j.c1.gameserver.model;
 import dev.crevan.l2j.c1.util.CopyOnWriteArrayList;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.logging.Logger;
 
-//TODO STUB
 public class L2Object implements Serializable {
+
+    public static final Logger log = Logger.getLogger(L2Object.class.getName());
 
     private int objectId;
     private int x;
     private int y;
     private int z;
+    private Set<L2PcInstance> knownPlayer = new HashSet<>();
 
     protected List<L2Object> knownObjectList = new CopyOnWriteArrayList<>();
 
@@ -53,5 +58,35 @@ public class L2Object implements Serializable {
 
     public void setKnownObjectList(final List<L2Object> knownObjectList) {
         this.knownObjectList = knownObjectList;
+    }
+
+    public void addKnownObject(final L2Object l2Object) {
+        knownObjectList.add(l2Object);
+
+        if (l2Object instanceof L2PcInstance) {
+            knownPlayer.add((L2PcInstance) l2Object);
+        }
+    }
+
+    public void removeKnownObject(final L2Object l2Object) {
+        knownObjectList.remove(l2Object);
+
+        if (l2Object instanceof L2PcInstance) {
+            knownPlayer.remove((L2PcInstance) l2Object);
+        }
+    }
+
+    public void removeAllKnownObjects() {
+        knownObjectList.forEach(l2Object -> l2Object.removeKnownObject(l2Object));
+
+        if (knownObjectList.size() != 0) {
+            log.info("KnownObjectList is not clear");
+            knownObjectList.clear();
+        }
+
+    }
+
+    public Set<L2PcInstance> getKnownPlayer() {
+        return knownPlayer;
     }
 }
