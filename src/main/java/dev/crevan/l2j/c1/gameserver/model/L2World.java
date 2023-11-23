148b9fa4b03675356ffc2e5e05eb7d757fb188ce
Crevan <a.siverskii@gmail.com>
2023-11-23 21:38:30 +0300

    add stub for L2World

---
+++ src/main/java/dev/crevan/l2j/c1/gameserver/model/L2World.java
@@ -0,0 +1,15 @@
+package dev.crevan.l2j.c1.gameserver.model;
+
+public class L2World {
+
+    // TODO
+    // STUB
+
+
+    public static L2World getInstance() {
+        return new L2World();
+    }
+
+    public void removeVisibleObject(final L2Character l2Character) {
+    }
+}
