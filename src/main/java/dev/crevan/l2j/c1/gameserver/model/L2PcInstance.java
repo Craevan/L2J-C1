b61104e90f02a8f2fa472445c4426639cb138c56
Crevan <a.siverskii@gmail.com>
2023-11-29 22:57:05 +0300

    update L2PcInstance

--- src/main/java/dev/crevan/l2j/c1/gameserver/model/L2PcInstance.java
+++ src/main/java/dev/crevan/l2j/c1/gameserver/model/L2PcInstance.java
@@ -1,5 +1,10 @@
 package dev.crevan.l2j.c1.gameserver.model;
 
+import dev.crevan.l2j.c1.gameserver.serverpackets.StatusUpdate;
+
 //TODO STUB
 public class L2PcInstance extends L2Character {
+    public void sendPacket(final StatusUpdate su) {
+        //todo
+    }
 }
